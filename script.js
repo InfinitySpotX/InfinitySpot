a791bf0b52c4b669f9e2028bdda0a33a97988263
InfinitySpotX <jjindustry001@gmail.com>
2026-07-02 09:15:30 +0530

    Update banner images in script.js
    Replaced banner image paths with new images.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Banner images
 const banners = [
-    "images/banner1.jpg",
-    "images/banner2.jpg",
-    "images/banner3.jpg",
-    "images/banner4.jpg"
+    "Aaro.jpg",
+    "Mareechika.jpg",
+    "Valayam.jpg",
+    "Maruvasham.jpg",
 ];
 
 // First image load
