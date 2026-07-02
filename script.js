9a8c6b7a338201b78022be96e426d0ad7b3d78b1
InfinitySpotX <jjindustry001@gmail.com>
2026-07-02 12:56:08 +0530

    Remove image carousel and add startApp function
    Removed image carousel functionality and added startApp function to redirect to home.html.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-let current = 0;
-const heroImage = document.getElementById("heroImage");
-
-heroImage.src = banners[current];
-
-setInterval(() => {
-    current = (current + 1) % banners.length;
-    heroImage.src = banners[current];
-}, 5000); // 5 seconds kazhinju image maarum
+function startApp() {
+    window.location.href = "home.html";
+}
